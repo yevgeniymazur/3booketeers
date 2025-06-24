72b1d8bb9463d909ef96123987122674e1474911
yevgeniymazur <119826811+yevgeniymazur@users.noreply.github.com>
2025-06-23 19:27:29 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,34 @@
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
-import './App.css';
+import './App.css'
 
-import Header     from './components/Header';
-import Footer     from './components/Footer';
-import Home       from './components/Home';
-import BookClub   from './components/BookClub';
-import Blog       from './components/Blog';
-import SignIn     from './components/SignIn';
-import SignUp     from './components/SignUp';
-import BookSearch from './components/BookSearch';
-import { AuthProvider } from './context/AuthContext'; // <-- Import AuthProvider
+import Header     from './components/Header'
+import Footer     from './components/Footer'
+import Home       from './components/Home'
+import BookClub   from './components/BookClub'
+import Blog       from './components/Blog'
+import SignIn     from './components/SignIn'
+import SignUp     from './components/SignUp'
+import BookSearch from './components/BookSearch'
 
 export default function App() {
-  console.log("App component rendered");
   return (
-    <AuthProvider> {/* <-- Wrap your app here */}
+    <>
       <Header />
-
       <main>
         <Routes>
           <Route path="/"         element={<Home />}       />
           <Route path="/bookclub" element={<BookClub />}   />
           <Route path="/blog"     element={<Blog />}       />
+          <Route path="/search"   element={<BookSearch />} />
           <Route path="/signin"   element={<SignIn />}     />
           <Route path="/signup"   element={<SignUp />}     />
-          <Route path="/search"   element={<BookSearch />} />
-
-          {/* catch-all: redirect anything else back to Home */}
-          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route path="*"         element={<Navigate to="/" replace />} />
         </Routes>
       </main>
-
       <Footer />
-    </AuthProvider>
-  );
+    </>
+  )
 }
+
